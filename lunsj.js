c451f7965d1ea6807107dbc5881345df3bcb87e7
Vegard Gamnes <vegard@gamnes.no>
2016-03-17 21:38:46 +0100

    Meny-dropdown, lunsjliste og betalingsoversikt.

--- lunsj.js
+++ lunsj.js
@@ -2,13 +2,36 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var fs = require('fs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var orders = [];
+var menuItems = [];
+
+fs.readFile(__dirname + '/menuItems.txt', 'utf8', function (err, data) {
+  if (err) {
+    return;
+  }
+  var lines = data.split('\n');
+  for(var i = 0; i < lines.length; i++) {
+  	var item = lines[i].split('###');
+  	if (item.length == 2) {
+		menuItems.push({
+			name: item[0],
+			price: item[1]
+		});
+  	}
+  }
+});
 
 function validReq(req) {
-	return req && req.body && req.body.name && req.body.order;
+	console.log(req.body);
+	return req && req.body && req.body.name && req.body.order && req.body.price;
+}
+
+function validMenuItemReq(req) {
+	return req && req.body && req.body.name && req.body.price;
 }
 
 app.get('/tine-lunsj', function (req, res) {
@@ -22,7 +45,7 @@ app.post('/tine-lunsj', function (req, res) {
 		var map = {
 			name: req.body.name,
 			order: req.body.order,
-			size: req.body.size,
+			price: req.body.price,
 			timestamp: new Date()
 		};
 		orders.push(map);
@@ -30,7 +53,41 @@ app.post('/tine-lunsj', function (req, res) {
 	}
 });
 
+app.get('/tine-lunsj/menuItem', function (req, res) {
+	res.sendfile(__dirname + '/menuItem.html');
+});
+
+app.post('/tine-lunsj/menuItem', function (req, res) {
+	if (!validMenuItemReq(req)) {
+		res.sendfile(__dirname + '/menuItemFailure.html');
+	} else {
+		var map = {
+			name: req.body.name,
+			price: req.body.price
+		};
+		menuItems.push(map);
+		fs.appendFile(__dirname + '/menuItems.txt', map.name + '###' + map.price + '\n', function (err) {
+			if (err) {
+				console.log('something bad happen - deal with this later..');	
+			}
+		});
+		res.sendfile(__dirname + '/menuItemSuccess.html');
+	}
+});
+
 app.get('/tine-lunsj/today', function (req, res) {
+	res.sendfile(__dirname + '/today.html');
+});
+
+app.get('/tine-lunsj/todayPayments', function (req, res) {
+	res.sendfile(__dirname + '/todayPayments.html');
+});
+
+app.get('/tine-lunsj/menuItems', function (req, res) {
+	res.send(menuItems);
+});
+
+app.get('/tine-lunsj/todayOrders', function (req, res) {
 	var ordersToday = [];
 	var today = new Date();
 	for (var i = 0; i < orders.length; i++) {
